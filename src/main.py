1f5e7053e3b7fe59051204abac978c9b795d1620
Allrthur <arthurbittencourt2@gmail.com>
2022-09-12 22:37:14 -0300

    pré-filtragem do dataset

--- src/main.py
+++ src/main.py
@@ -1,2 +1,20 @@
-f = open("files/dataset.txt", "r")
-print(f.read())
+import re
+
+def filtrar_linhas(l):
+    return l.split("	")[0:2]
+
+dataset = open("files/dataset.txt", "r", encoding="utf8")
+
+lines = dataset.read().split("\n")
+
+filtered_lines = list(map(filtrar_linhas, lines))
+
+#TODO: Retirar a pontuação de cada par de palavras
+
+print(filtered_lines[0:1000])
+
+
+
+
+
+
