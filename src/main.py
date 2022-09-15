bd6f08f81f6fd72ee050a932cc909f24ca95564c
Allrthur <arthurbittencourt2@gmail.com>
2022-09-14 22:07:46 -0300

    pré-filtragem do dataset com regex

--- src/main.py
+++ src/main.py
@@ -3,15 +3,20 @@ import re
 def filtrar_linhas(l):
     return l.split("	")[0:2]
 
+def retirar_pontuacao_string(s): #  retira o ponto de uma string singular usando regex
+    return re.sub('[^A-Za-z0-9]+', ' ', s) # TODO: Ajustar o regex para incluir acentuações(?)
+
+def retirar_pontuacao(f): # retira a pontuacao de uma lista de strings (par original, traducao)
+    result = list(map(retirar_pontuacao_string, f))
+    return result
+
 dataset = open("files/dataset.txt", "r", encoding="utf8")
 
 lines = dataset.read().split("\n")
 
-filtered_lines = list(map(filtrar_linhas, lines))
+filtered_lines = list(map(retirar_pontuacao, list(map(filtrar_linhas, lines))))
 
-#TODO: Retirar a pontuação de cada par de palavras
-
-print(filtered_lines[0:1000])
+print(filtered_lines)
 
 
 
